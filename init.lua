887118aa65827aed6463a7e67988e4acd0e3877b
fzarco <filipezarcocoutinho@gmail.com>
2022-10-23 02:25:32 +0100

    asdasd

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
+-- Core
 require("fzarco.core.options")
 require("fzarco.core.colorscheme")
 require("fzarco.core.keymaps")
+
+-- Packer
 require("fzarco.plugins-setup")
+
+-- Plugins
 require("fzarco.plugins.lualine")
 require("fzarco.plugins.telescope")
 require("fzarco.plugins.nvim-cmp")
@@ -9,5 +14,7 @@ require("fzarco.plugins.autopairs")
 require("fzarco.plugins.treesitter")
 require("fzarco.plugins.gitsigns")
 require("fzarco.plugins.nvim-tree")
+
+-- LSP
 require("fzarco.plugins.lsp.mason")
 require("fzarco.plugins.lsp.lspconfig")
